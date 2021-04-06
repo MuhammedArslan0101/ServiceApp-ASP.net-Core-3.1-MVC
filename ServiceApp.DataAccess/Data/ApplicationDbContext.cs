6a95526a2f9bcfb1e8bb5f75e3bce108380d3e97
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-04-06 10:34:58 +0300

    Organizing code in N-tiers architecture

--- ServiceApp.DataAccess/Data/ApplicationDbContext.cs
+++ ServiceApp.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ServiceApp.Data
+namespace ServiceApp.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
