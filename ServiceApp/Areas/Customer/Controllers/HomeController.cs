d2cdbed78387fcce6cd1fd644be125d20c0fffa6
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-04-06 10:20:11 +0300

    Adding Areas as Admin and Customer and make change to the Folders in project

--- ServiceApp/Areas/Customer/Controllers/HomeController.cs
+++ ServiceApp/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using ServiceApp.Models;
 
 namespace ServiceApp.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- ServiceApp/Startup.cs
+++ ServiceApp/Startup.cs
@@ -62,7 +62,7 @@ namespace ServiceApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
